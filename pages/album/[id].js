2ffb360667cfdea8d0a5669e4a41c3935f9c9ded
RealPenguin <mrjanggo858@gmail.com>
2021-10-08 10:38:34 +0700

    stuff

---
+++ pages/album/[id].js
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+
+export default function AlbumPage({ album }) {
+  return (
+    <div>   
+            <Head>
+                    <title>{album.name}</title>
+            </Head>
+    </div>
+  )
+}
+
+/**
+* 
+* @param {import('next').GetServerSidePropsContext} context
+* @returns
+*/
+export async function getServerSideProps(context) {
+    const res = await fetch(`${process.env.BASE_URL}/api/album/${context.params.id}`)
+    const album = await res.json()
+
+    return {
+        props: {
+           album
+        }
+    }
+}
